7f52a8d8ebeb071f881fc19fce89091b72d3db99
Akash Badshah <akashbad@mit.edu>
2012-10-06 17:37:47 -0400

    added rails app and postgres setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flare::Application.config.secret_token = '91a5bdae3493a9e843f2fab83593a30606bc6bef5877b2ce524b132b5c32f1307aca74c5fb2132d500e31b4ebdd13442be04eb5f9033a2e5ab33dff2294a3be7'
